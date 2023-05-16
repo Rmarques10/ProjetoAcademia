2cf422ce5c038aa9e0d5e8fa0a7ec686e083b6f3
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-05-16 12:45:07 -0300

    Criação do projeto acadmia

---
+++ Academias.Infra.Data/Context/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Academias.Infra.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
